95f0b4507dd5fd483e43bd1aab7dd7d4fc75abd8
John Idowu <johntoby007@gmail.com>
2024-07-16 07:39:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,10 @@ resource "aws_instance" "webserver" {
 
 
 resource "aws_instance" "databaseserver" {
-  ami = "ami-0ff591da048329e00"
+  provider      =  aws.west
+  ami           = "ami-0ff591da048329e00"
   instance_type = "t2.micro"
+  
 
    tags = {                                
      Name = "databaseserver"
